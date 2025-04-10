5ebd3c539a80a7a8bd5c5ec7530325cd5fbe266a
Samithsr <samithrgowda@gmail.com>
2025-04-10 11:17:43 +0530

    addind sujan

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-const express = require("express")
-const cors = require("cors")
-
+const express = require('express')
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(cors({
-    origin : "*",
-    methods : ["GET","POST","PUT","PATCH","DELETE"]
-}))
 
-app.get("/",(req,res)=>{
-    res.status(200).json({success:true,message:"Hello World 15"})
+app.get('/',(req,res)=>{
+    res.json({
+        mesg:'Hello Sujan'
+    })
 })
 
-app.listen(5000,()=>{
-    console.log("Listening on port 5000");
-})
+app.listen(5000)
